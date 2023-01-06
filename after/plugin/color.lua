30b395cb2d4d723a33c3b4f44b903bfc33f42964
Oleh V <oleh@olehmac16.local>
2023-01-06 18:41:53 +0200

    Remove grey background for sign column(left from number column)

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,7 +1,8 @@
 function SetColor(color)
 	color = color or "angr"
 	vim.cmd.AirlineTheme(color)
-    vim.cmd.colorscheme("desert")
+    vim.cmd { cmd = "highlight", args = { "clear", "SignColumn" } }
+    --vim.cmd.colorscheme("desert")
 	
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})

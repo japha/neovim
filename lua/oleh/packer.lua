7fc819db6a16d2cb9321e4a1e012fc0a361561f0
Oleh V <oleh@olehmac16.local>
2023-02-07 17:57:07 +0200

    Some changes for MarkdownPreview

--- lua/oleh/packer.lua
+++ lua/oleh/packer.lua
@@ -60,7 +60,7 @@ return require('packer').startup(function(use)
       run = function() vim.fn["mkdp#util#install"]() end,
   })
 
-  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+  --use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
   use { "preservim/vim-markdown" }
   use { "mzlogin/vim-markdown-toc" }
 end)

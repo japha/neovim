b660a483658a698817b5a02b7763daf66a60c24b
Oleh V <olehas@gmail.com>
2023-11-22 21:35:26 +0200

    Use system clangd which knows about standard c++ library and headers location

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,79 +1,53 @@
-local lsp = require("lsp-zero")
 local lsp_config = require("lspconfig")
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
--- Fix Undefined global 'vim'
-lsp.configure('lua_ls', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-})
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP Actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
 
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
-
--- disable completion with tab
--- this helps with copilot setup
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
-
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
-})
-
-lsp_config.clangd.setup({
-  on_new_config = function(config, root_dir)
-    config.cmd = {
-      "clangd",
-      "--background-index",
-      "--query_driver",
-      "/usr/bin/clang++"
-    }
+    vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+    vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+    vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
   end
 })
 
-lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+require('mason').setup()
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'clangd',
+    'rust_analyzer',
+    'lua_ls'
+  }
+})
 
-
-  if client.name == "eslint" then
-      vim.cmd.LspStop('eslint')
-      return
+local servers = require('mason-lspconfig').get_installed_servers
+for _, server in ipairs(servers()) do
+  local config = { capabilities = lsp_capabilities }
+  if server == 'clangd' then
+    config.on_new_config = function(config, root_dir)
+      config.cmd = {
+        "/Library/Developer/CommandLineTools/usr/bin/clangd",
+        "--background-index",
+        "--query-driver",
+        "/Library/Developer/CommandLineTools/usr/bin/clang++"
+      }
+    end
   end
+  lsp_config[server].setup(config)
+end
 
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-  vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-  vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-  vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-end)
-
-lsp.setup()
 
 -- diagnostics settings
 vim.diagnostic.config({
@@ -85,5 +59,5 @@ vim.diagnostic.config({
   float = true,
 })
 
---vim.lsp.set_log_level("debug")
+--vim.lsp.set_log_level("trace")
 

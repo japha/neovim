86c004f2b9fed1277faa88156a16cfac433f130f
Oleh V <olehas@gmail.com>
2025-05-25 10:30:22 +0300

    Update some color theme stuff

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,13 +1,13 @@
 function SetColor(color)
 	color = color or "angr"
 	vim.cmd.AirlineTheme(color)
-    vim.cmd { cmd = "highlight", args = { "clear", "SignColumn" } }
-	
-	vim.api.nvim_set_hl(0, "Normal", { bg = "#0a0a0a" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#0a0a0a"})
+	vim.cmd { cmd = "highlight", args = { "clear", "SignColumn" } }
 
-    vim.cmd("set background=dark")
-    vim.cmd.colorscheme("angr")
+	vim.cmd.AirlineTheme("wombat")
+	vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
+
+	--vim.cmd("set background=NONE")
 end
 
 SetColor("angr")
--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -1,90 +1,89 @@
 return { 
-	{ 'wbthomason/packer.nvim' },
-        {'nvim-lua/plenary.nvim'},
-        {'nvim-tree/nvim-web-devicons'},
+	{'nvim-lua/plenary.nvim'},
+	{'nvim-tree/nvim-web-devicons'},
 
-  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', },
+	{ 'nvim-telescope/telescope.nvim', branch = '0.1.x', },
 
-  {'nvim-telescope/telescope-fzf-native.nvim'},
-  {'nvim-treesitter/nvim-treesitter'},
-  {'nvim-treesitter/playground'},
+	{'nvim-telescope/telescope-fzf-native.nvim'},
+	{'nvim-treesitter/nvim-treesitter'},
+	{'nvim-treesitter/playground'},
 
-  --Source tree
-  {'nvim-tree/nvim-tree.lua'},
+	--Source tree
+	{'nvim-tree/nvim-tree.lua'},
 
--- color schemes
-  {'vim-airline/vim-airline'},
-  {'vim-airline/vim-airline-themes'},
-  {'zacanger/angr.vim'},
+	-- color schemes
+	{'vim-airline/vim-airline'},
+	{'vim-airline/vim-airline-themes'},
+	{'zacanger/angr.vim'},
 
--- code formatter
-  {'mhartington/formatter.nvim'},
+	-- code formatter
+	{'mhartington/formatter.nvim'},
 
-  {'mbbill/undotree'},
-  {'tpope/vim-fugitive'},
+	{'mbbill/undotree'},
+	{'tpope/vim-fugitive'},
 
-  -- LSP (Language Server Protocol)
-  -- LSP Support
-  {'neovim/nvim-lspconfig'},
-  {'williamboman/mason.nvim'},
-  {'williamboman/mason-lspconfig.nvim'},
+	-- LSP (Language Server Protocol)
+	-- LSP Support
+	{'neovim/nvim-lspconfig'},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
 
-  -- Autocompletion
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
-  {'hrsh7th/cmp-buffer'},
-  {'hrsh7th/cmp-path'},
-  {'saadparwaiz1/cmp_luasnip'},
-  {'hrsh7th/cmp-nvim-lua'},
+	-- Autocompletion
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'hrsh7th/cmp-buffer'},
+	{'hrsh7th/cmp-path'},
+	{'saadparwaiz1/cmp_luasnip'},
+	{'hrsh7th/cmp-nvim-lua'},
 
-  --Snippets
-  {'L3MON4D3/LuaSnip'},
-  {'rafamadriz/friendly-snippets'},
+	--Snippets
+	{'L3MON4D3/LuaSnip'},
+	{'rafamadriz/friendly-snippets'},
 
-  {
-      "folke/trouble.nvim",
-      requires = "kyazdani42/nvim-web-devicons",
-      config = function()
-          require("trouble").setup {
-              -- your configuration comes here
-              -- or leave it empty to the default settings
-              -- refer to the configuration section below
-          }
-      end
-  },
+	{
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup {
+				-- your configuration comes here
+				-- or leave it empty to the default settings
+				-- refer to the configuration section below
+			}
+		end
+	},
 
-  -- cppman
-  {
-    'madskjeldgaard/cppman.nvim',
-    requires = {
-      { 'MunifTanjim/nui.nvim' }
-    },
-    config = function()
-      local cppman = require"cppman"
-      cppman.setup()
+	-- cppman
+	{
+		'madskjeldgaard/cppman.nvim',
+		requires = {
+			{ 'MunifTanjim/nui.nvim' }
+		},
+		config = function()
+			local cppman = require"cppman"
+			cppman.setup()
 
-    end
-  },
+		end
+	},
 
-  { "puremourning/vimspector" },
+	{ "puremourning/vimspector" },
 
-  { "godlygeek/tabular" },
-  -- install without yarn or npm
-  {{
-      "iamcco/markdown-preview.nvim",
-      run = function() vim.fn["mkdp#util#install"]() end,
-  }},
+	{ "godlygeek/tabular" },
+	-- install without yarn or npm
+	{{
+		"iamcco/markdown-preview.nvim",
+		run = function() vim.fn["mkdp#util#install"]() end,
+	}},
 
-  --{{ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
-  { "preservim/vim-markdown" },
-  { "mzlogin/vim-markdown-toc" },
+	--{{ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+	{ "preservim/vim-markdown" },
+	{ "mzlogin/vim-markdown-toc" },
 
-  { "cdelledonne/vim-cmake" },
+	{ "cdelledonne/vim-cmake" },
 
-  --co-pilot test
-  -- { "github/copilot.vim" }
+	--co-pilot test
+	-- { "github/copilot.vim" }
 
-  -- rust support
-  { "simrat39/rust-tools.nvim" },
-  { "voldikss/vim-floaterm" }
-  }
+	-- rust support
+	{ "simrat39/rust-tools.nvim" },
+	{ "voldikss/vim-floaterm" }
+}
